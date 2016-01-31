6a8e614376d687d179de6dab290497778758c45c
k16.shikano <k16.shikano@gmail.com>
2016-01-31 15:17:59 +0900

    gather sample programs in a file. no longer has main.hs

---
+++ Sample.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Pdf
+import PdfObj
+
+import Data.ByteString.UTF8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+
+-- import Debug.Trace
+
+-- initstate = (0,0,70,660)
+initstate = PSR { linex=0
+                , liney=0
+                , absolutex=0
+                , absolutey=700
+                , leftmargin=0.0
+                , top=700.0
+                , bottom=0.0
+                , curfont=""
+                , fontmaps=[]}
+
+
+-----------------------------------------------
+-- Show each PDF Object by its reference number
+-----------------------------------------------
+
+objectByRef filename ref = getPdfObjByRef ref (getPDFObjFile filename)
+
+contentByRef filename ref = do
+  objs <- getPDFObjFile filename
+  obj <- objectByRef filename ref
+  BSL.putStrLn $ showPageContent obj objs
+
+showPageContent obj objs =
+    case findDictOfType "/Page" obj of
+      Just dict -> contentsStream dict initstate objs
+      Nothing -> ""
+
+
+---------------------------------------
+-- Sort Object References in Page order
+---------------------------------------
+
+data  PageTree = Nop | Page Int | Pages [PageTree]
+                 deriving Show
+
+refByPage filename = do
+  contents <- BS.readFile filename
+  let objs = map parsePDFObj $ getObjs contents
+  let rootref = case rootRef contents of
+                  Just r  -> r
+                  Nothing -> 0
+  putStrLn $ show $ pageTreeToList $ pageorder rootref objs
+
+pageorder :: Int -> [PDFObj] -> PageTree
+pageorder parent objs = 
+  case findObjsByRef parent objs of
+    Just os -> case findDictOfType "/Catalog" os of
+      Just dict -> case pages dict of 
+        Just pr -> pageorder pr objs
+        Nothing -> Nop
+      Nothing -> case findDictOfType "/Pages" os of
+        Just dict -> case pagesKids dict of
+          Just kidsrefs -> Pages $ map (\f -> f objs) (map pageorder kidsrefs)
+          Nothing -> Nop
+        Nothing -> case findDictOfType "/Page" os of
+          Just dict -> Page parent
+          Nothing -> Nop
+    Nothing -> Nop
+
+pageTreeToList :: PageTree -> [Int]
+pageTreeToList (Pages ps) = concatMap pageTreeToList ps
+pageTreeToList (Page n) = [n]
+pageTreeToList Nop = []
+
+
+--------------------------
+-- Get Whole Text from PDF
+--------------------------
+-- First: grub objects
+-- Second: parse within each object, deflating its stream
+-- Third: linearize stream
+
+pdfToText filename = do
+  contents <- BS.readFile filename
+  let objs = map parsePDFObj $ getObjs contents
+  let rootref = case rootRef contents of
+                  Just r  -> r
+                  Nothing -> 0
+  putStrLn $ show $ linearize rootref objs
+
+linearize :: Int -> [PDFObj] -> PDFStream
+linearize parent objs = 
+  case findObjsByRef parent objs of
+    Just os -> case findDictOfType "/Catalog" os of
+      Just dict -> case pages dict of 
+        Just pr -> linearize pr objs
+        Nothing -> ""
+      Nothing -> case findDictOfType "/Pages" os of
+        Just dict -> case pagesKids dict of
+          Just kidsrefs -> BSL.concat $ map (\f -> f objs) (map linearize kidsrefs)
+          Nothing -> ""
+        Nothing -> case findDictOfType "/Page" os of
+          Just dict -> contentsStream dict initstate objs
+          Nothing -> ""
+    Nothing -> ""
+
